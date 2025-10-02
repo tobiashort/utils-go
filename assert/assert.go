65c866c7cb554e95a1e3aec3ee4d3ff614a1c8aa
Tobias Hort <mail@tobiashort.com>
2025-10-02 06:11:26 +0200

    panicFunc

--- assert/assert.go
+++ assert/assert.go
@@ -4,50 +4,54 @@ import (
 	"fmt"
 )
 
+var PanicFunc = func(v any) {
+	panic(v)
+}
+
 func Nil(v any, msg string) {
 	if v != nil {
-		panic(fmt.Errorf("%s", msg))
+		PanicFunc(fmt.Errorf("%s", msg))
 	}
 }
 
 func Nilf(v any, format string, args ...any) {
 	if v != nil {
-		panic(fmt.Errorf(format, args...))
+		PanicFunc(fmt.Errorf(format, args...))
 	}
 }
 
 func NotNil(v any, msg string) {
 	if v == nil {
-		panic(fmt.Errorf("%s", msg))
+		PanicFunc(fmt.Errorf("%s", msg))
 	}
 }
 
 func NotNilf(v any, format string, args ...any) {
 	if v == nil {
-		panic(fmt.Errorf(format, args...))
+		PanicFunc(fmt.Errorf(format, args...))
 	}
 }
 
 func True(cond bool, msg string) {
 	if !cond {
-		panic(fmt.Errorf("%s", msg))
+		PanicFunc(fmt.Errorf("%s", msg))
 	}
 }
 
 func Truef(cond bool, format string, args ...any) {
 	if !cond {
-		panic(fmt.Errorf(format, args...))
+		PanicFunc(fmt.Errorf(format, args...))
 	}
 }
 
 func False(cond bool, msg string) {
 	if cond {
-		panic(fmt.Errorf("%s", msg))
+		PanicFunc(fmt.Errorf("%s", msg))
 	}
 }
 
 func Falsef(cond bool, format string, args ...any) {
 	if cond {
-		panic(fmt.Errorf(format, args...))
+		PanicFunc(fmt.Errorf(format, args...))
 	}
 }
