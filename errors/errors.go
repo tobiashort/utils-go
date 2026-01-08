af84b247a513a04dcabb92f6f3cc3e7c89b73e0e
Tobias Hort <mail@tobiashort.com>
2026-01-08 17:31:33 +0100

    more context

--- errors/errors.go
+++ errors/errors.go
@@ -7,17 +7,29 @@ import (
 )
 
 func WithCtx(text string) error {
-	_, file, line, ok := runtime.Caller(1)
-	if ok {
-		text = fmt.Sprintf("%s\n  at %s:%d", text, file, line)
+	skip := 0
+	for {
+		skip++
+		_, file, line, ok := runtime.Caller(skip)
+		if ok {
+			text = fmt.Sprintf("%s\n  at %s:%d", text, file, line)
+		} else {
+			break
+		}
 	}
 	return errors2.New(text)
 }
 
 func WithCtxf(format string, a ...any) error {
-	_, file, line, ok := runtime.Caller(1)
-	if ok {
-		format = fmt.Sprintf("%s\n  at %s:%d", format, file, line)
+	skip := 0
+	for {
+		skip++
+		_, file, line, ok := runtime.Caller(skip)
+		if ok {
+			format = fmt.Sprintf("%s\n  at %s:%d", format, file, line)
+		} else {
+			break
+		}
 	}
 	return fmt.Errorf(format, a...)
 }
